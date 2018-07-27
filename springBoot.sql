e4e3a6175a186fb2e47707f70ab75a1c80019388
chenguangjie <chenguangjie@zhuojianchina.com>
2018-07-27 14:01:16 +0800

    测试自动生成表

--- springBoot.sql
+++ springBoot.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2018-07-26 19:22:51
+Date: 2018-07-27 14:00:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,4 +23,15 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   `user_password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for wx_role
+-- ----------------------------
+DROP TABLE IF EXISTS `wx_role`;
+CREATE TABLE `wx_role` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `role_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
